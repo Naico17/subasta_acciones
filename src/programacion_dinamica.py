08a8bb2faff3f2ec7eb21b11acdee2e3232046a0
defCamilo <jmino@unal.edu.co>
2025-12-01 14:38:58 -0500

    Update programacion_dinamica.py

--- src/programacion_dinamica.py
+++ src/programacion_dinamica.py
@@ -1,4 +1,4 @@
-def SubastaAccionesDP(A, N, l, u, p):
+def dynamic_programming(A, N, l, u, p):
     """
     Programación dinámica iterativa para el problema de subasta de acciones.
 
@@ -21,25 +21,24 @@ def SubastaAccionesDP(A, N, l, u, p):
     dp = [[0 for _ in range(A + 1)] for _ in range(N + 1)]
 
     # Caso base: i = 0 --> ganancia nula (ya está con ceros)
-    for a in range(A + 1):
-        dp[0][a] = 0
+    for shares in range(A + 1):
+        dp[0][shares] = 0
 
     # Llenar la tabla fila por fila
     for i in range(1, N + 1):
-        for a in range(A + 1):
+        for shares in range(A + 1):
             # Opción 1: no asignar acciones al oferente i
-            mejor = dp[i - 1][a]
+            best = dp[i - 1][shares]
 
             # Opción 2: asignar x acciones al oferente i
-            if a >= l[i - 1]:
-                limite = min(u[i - 1], a)
-                for x in range(l[i - 1], limite + 1):
-                    ganancia = p[i - 1] * x + dp[i - 1][a - x]
-                    if ganancia > mejor:
-                        mejor = ganancia
+            if shares >= l[i - 1]:
+                limit = min(u[i - 1], shares)
+                for x in range(l[i - 1], limit + 1):
+                    profit = p[i - 1] * x + dp[i - 1][shares - x]
+                    if profit > best:
+                        best = profit
 
-            dp[i][a] = mejor
+            dp[i][shares] = best
 
     # La ganancia máxima se encuentra en dp[N][A]
     return dp[N][A]
-
