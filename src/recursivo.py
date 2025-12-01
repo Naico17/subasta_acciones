89f3119a8f1d505a22af79c0a0ea27c4bbd62053
defCamilo <jmino@unal.edu.co>
2025-12-01 14:42:09 -0500

    Update recursivo.py

--- src/recursivo.py
+++ src/recursivo.py
@@ -1,33 +1,33 @@
-def ganancia_maxima(i, a, l, u, p):
+def max_profit_recursive(i, shares, l, u, p):
     """
     Versión recursiva pura.
 
     i : cantidad de oferentes considerados (1..N)
-    a : acciones disponibles para repartir (0..A)
+    shares : acciones disponibles para repartir (0..A)
     """
     # Caso base: no hay oferentes o no hay acciones
-    if i == 0 or a == 0:
+    if i == 0 or shares == 0:
         return 0
 
     # Opción 1: no asignar acciones al oferente i
-    mejor = ganancia_maxima(i - 1, a, l, u, p)
+    best = max_profit_recursive(i - 1, shares, l, u, p)
 
     # Opción 2: asignar x acciones al oferente i-1
-    li = l[i - 1]
-    ui = u[i - 1]
+    min_i = l[i - 1]
+    max_i = u[i - 1]
 
-    if a >= li:
-        limite_superior = min(ui, a)
-        for x in range(li, limite_superior + 1):
-            valor = p[i - 1] * x + ganancia_maxima(i - 1, a - x, l, u, p)
-            if valor > mejor:
-                mejor = valor
+    if shares >= min_i:
+        upper_limit = min(max_i, shares)
+        for x in range(min_i, upper_limit + 1):
+            value = p[i - 1] * x + max_profit_recursive(i - 1, shares - x, l, u, p)
+            if value > best:
+                best = value
 
-    return mejor
+    return best
 
 
-def SubastaAccionesRecursiva(A, N, l, u, p):
+def recursive(A, N, l, u, p):
     """
     Función de envoltura para llamar a la versión recursiva pura.
     """
-    return ganancia_maxima(N, A, l, u, p)
+    return max_profit_recursive(N, A, l, u, p)
