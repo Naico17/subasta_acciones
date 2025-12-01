91983e84227c4fb04011902d2d354144d4cbf6f9
defCamilo <jmino@unal.edu.co>
2025-12-01 14:40:38 -0500

    Update recursivaMemorización.py

--- src/recursivaMemorización.py
+++ src/recursivaMemorización.py
@@ -1,32 +1,33 @@
 from functools import lru_cache
 
-def SubastaAccionesRecursivaMemo(A, N, l, u, p):
+
+def recursive_memoization(A, N, l, u, p):
     """
     Versión recursiva con memoización (top-down) para la subasta de acciones.
     Usa un caché interno para guardar los subproblemas (i, a).
     """
 
     @lru_cache(maxsize=None)
-    def aux(i, a):
+    def memo_helper(i, shares):
         # Caso base: no hay oferentes o no hay acciones
-        if i == 0 or a == 0:
+        if i == 0 or shares == 0:
             return 0
 
         # Opción 1: no asignar acciones al oferente i
-        mejor = aux(i - 1, a)
+        best = memo_helper(i - 1, shares)
 
         # Opción 2: asignar x acciones al oferente i-1
-        li = l[i - 1]
-        ui = u[i - 1]
+        min_i = l[i - 1]
+        max_i = u[i - 1]
 
-        if a >= li:
-            limite_superior = min(ui, a)
-            for x in range(li, limite_superior + 1):
-                valor = p[i - 1] * x + aux(i - 1, a - x)
-                if valor > mejor:
-                    mejor = valor
+        if shares >= min_i:
+            upper_limit = min(max_i, shares)
+            for x in range(min_i, upper_limit + 1):
+                value = p[i - 1] * x + memo_helper(i - 1, shares - x)
+                if value > best:
+                    best = value
 
-        return mejor
+        return best
 
     # Llamada inicial: considerar N oferentes y A acciones
-    return aux(N, A)
+    return memo_helper(N, A)
